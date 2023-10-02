610416cfdcda9f566fee63de9cc870a1b23f79aa
Oswaldo Junior <osnjunior91@gmail.com>
2023-10-02 16:27:07 -0300

    Ajuste na conversao do tempo de execucao

--- iTaaSSolution/iTaaSSolution/src/iTaaSLogConverter.Infrastructure/Utilities/Extensions/StringExtensions.cs
+++ iTaaSSolution/iTaaSSolution/src/iTaaSLogConverter.Infrastructure/Utilities/Extensions/StringExtensions.cs
@@ -1,10 +1,12 @@
-﻿namespace CandidateTesting.OswaldoDaSilvaNicacioJunior.src.iTaaSLogConverter.Infrastructure.Utilities.Extensions
+﻿using System.Globalization;
+
+namespace CandidateTesting.OswaldoDaSilvaNicacioJunior.src.iTaaSLogConverter.Infrastructure.Utilities.Extensions
 {
     public static class StringExtensions
     {
         public static string RoundToDecimalString(this string input)
         {
-            if (decimal.TryParse(input, out decimal number))
+            if (decimal.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal number))
             {
                 int roundedNumber = (int)Math.Round(number, MidpointRounding.AwayFromZero);
                 return roundedNumber.ToString();
